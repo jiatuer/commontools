2a0d5703578520238d1a5e9cbf089c4bb7417002
thinkinghunter <jiatuer@email.com>
2013-11-21 23:27:06 +0800

    sshexecute.jar

---
+++ src/com/tool/sshexecute/LinuxTool.java
@@ -0,0 +1,66 @@
+package com.tool.sshexecute;
+
+import net.neoremind.sshxcute.core.ConnBean;
+import net.neoremind.sshxcute.core.Result;
+import net.neoremind.sshxcute.core.SSHExec;
+import net.neoremind.sshxcute.exception.TaskExecFailException;
+import net.neoremind.sshxcute.task.CustomTask;
+import net.neoremind.sshxcute.task.impl.ExecCommand;
+import net.neoremind.sshxcute.task.impl.ExecShellScript;
+
+public class LinuxTool {
+
+	
+
+	public static void main(String[] args) throws TaskExecFailException {
+//		 Initialize a SSHExec instance without referring any object. 
+		SSHExec ssh = null;
+		// Wrap the whole execution jobs into try-catch block   
+		try {
+		    // Initialize a ConnBean object, parameter list is ip, username, password
+		    ConnBean cb = new ConnBean("192.168.119.134", "admin","admin");
+		    // Put the ConnBean instance as parameter for SSHExec static method getInstance(ConnBean) to retrieve a real SSHExec instance
+		    ssh = SSHExec.getInstance(cb);              
+		    // Create a ExecCommand, the reference class must be CustomTask
+		    CustomTask ct1 = new ExecCommand("cd file");
+		    // Create a ExecShellScript, the reference class must be CustomTask
+//		    CustomTask ct2 = new ExecShellScript("/home/tsadmin","./sshxcute_test.sh","hello world");
+		    // Connect to server
+		    ssh.connect();
+		    // Upload sshxcute_test.sh to /home/tsadmin on remote system
+//		    ssh.uploadSingleDataToServer("data/sshxcute_test.sh", "/home/tsadmin");
+		    // Execute task
+		    // Execute task and get the returned Result object
+		    Result res = ssh.exec(ct1);
+//		    Result res = ssh.exec(ct2);
+		    // Check result and print out messages.
+		    if (res.isSuccess)
+		    {
+		        System.out.println("Return code: " + res.rc);
+		        System.out.println("sysout: " + res.sysout);
+		    }
+		    else
+		    {
+		        System.out.println("Return code: " + res.rc);
+		        System.out.println("error message: " + res.error_msg);
+		    }
+		} 
+		catch (TaskExecFailException e) 
+		{
+		    System.out.println(e.getMessage());
+		    e.printStackTrace();
+		} 
+		catch (Exception e) 
+		{
+		    System.out.println(e.getMessage());
+		    e.printStackTrace();
+		} 
+		finally 
+		{
+		    ssh.disconnect();   
+		}
+		
+	}
+	
+	
+}
